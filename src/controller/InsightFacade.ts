fb4cf4060533f10f67e0af16dcb93ec1a968e1be
Haley Russell <haleyru@students.cs.ubc.ca>
2022-02-11 00:35:19 -0800

    Figured out how to extract relevent info from JSONs, #12

--- src/controller/InsightFacade.ts
+++ src/controller/InsightFacade.ts
@@ -1,7 +1,7 @@
 import {IInsightFacade, InsightDataset, InsightDatasetKind, InsightError, InsightResult} from "./IInsightFacade";
 
 import {isValidDatasetIdName} from "../../src/utils/DatasetUtils";
-
+import JSZip from "jszip";
 /**
  * This is the main programmatic entry point for the project.
  * Method documentation is in IInsightFacade
@@ -14,11 +14,41 @@ export default class InsightFacade implements IInsightFacade {
 
 	public addDataset(id: string, content: string, kind: InsightDatasetKind): Promise<string[]> {
 		if (!isValidDatasetIdName(id)) {
-			return Promise.reject (new InsightError("Given an invalid id " + id));
+			return Promise.reject(new InsightError("Given an invalid id " + id));
 		}
 		if (kind === InsightDatasetKind.Rooms) {
-			return Promise.reject (new InsightError("Given dataset kind Rooms"));
+			return Promise.reject(new InsightError("Given dataset kind Rooms"));
 		}
+
+		// Todo: Properly configure promises below & figure out non zip case
+		JSZip.loadAsync(content, {base64: true}).then(function (zip) {
+			zip.folder("courses")?.forEach(((relativePath, file) => {
+				file.async("string").then((contents) => {
+					const obj = JSON.parse(contents);
+					const result = obj.result;
+					for (let i = 0; i <= result.length; i++) {
+						let dept = result[i].Subject;
+						let id = result[i].Course;
+						let avg = result[i].Avg;
+						let instructor = result[i].Professor;
+						let title = result[i].Title;
+						let pass = result[i].Pass;
+						let fail = result[i].Fail;
+						let audit = result[i].Audit;
+						let uuid = result[i].id;
+						let year = result[i].Year;
+						if (result[i].Section === "overall") {
+							year = "1900";
+						}
+						if (dept !== undefined && id !== undefined && avg !== undefined &&
+							instructor !== undefined && title !== undefined && pass !== undefined &&
+							fail !== undefined && audit !== undefined && uuid !== undefined && year !== undefined) {
+							// Todo: Save valid fields to some data structure
+						}
+					}
+				});
+			}));
+		});
 		return Promise.reject("Not implemented.");
 	}
 

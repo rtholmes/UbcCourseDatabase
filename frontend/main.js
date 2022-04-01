9a998ecdab80e4f7ebf8cb0e06563cf0525c3209
Haley Russell <haleyru@students.cs.ubc.ca>
2022-03-31 20:12:15 -0700

    Implemented handleAddQuery and handleListQuery for frontend, just need handleRunQuery #47

--- frontend/main.js
+++ frontend/main.js
@@ -1,22 +1,14 @@
 const Discord = require('discord.js');
-
 const Bot = new Discord.Client({ intents: ["GUILDS", "GUILD_MESSAGES"] });
+const fetch = require("node-fetch");
 
 const prefix = "!";
-let queries = [];
-let answered1;
-let answered2;
-let answered3;
-let answered4;
 
 Bot.once('ready', () => {
 	console.log("C3 Bot is online!");
-	const testQueryInsert = ["Test Query", '{  "WHERE": {    "EQ": {      "rooms_seats": 0    }  },  "OPTIONS": {    "COLUMNS": [      "rooms_name"    ]  }}'];
-
-	queries.push(testQueryInsert);
 })
 
-Bot.on('message', message => {
+Bot.on('messageCreate', message => {
 	if(!message.content.startsWith(prefix) || message.author.bot) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/)
@@ -28,10 +20,10 @@ Bot.on('message', message => {
 	}
 
 	function mainMenu() {
-		answered1 = false;
-		message.channel.send('Please choose an option:');
+		let answered1 = false;
+		message.channel.send('Please choose an option number:');
 		message.channel.send('[1] Add query \n[2] List queries \n[3] Run Query \n');
-		Bot.on('message', async message => {
+		Bot.on('messageCreate', async message => {
 			if (answered1) return;
 			const args = message.content.split(/ +/)
 			const command = args.shift().toLowerCase();
@@ -52,9 +44,9 @@ Bot.on('message', message => {
 	}
 
 	function handleAddQuery() {
-		answered2 = false;
+		let answered2 = false;
 		message.channel.send("Please insert a unique query name: ");
-		Bot.on('message', async message => {
+		Bot.on('messageCreate', async message => {
 			if(message.author.bot || answered2) return;
 			answered2 = true;
 			handleQueryFile(message.content);
@@ -62,56 +54,76 @@ Bot.on('message', message => {
 	}
 
 	function handleQueryFile(name) {
-		answered3 = false;
+		let answered3 = false;
 		message.channel.send("Please input JSON query: ");
-		Bot.on('message', async message => {
+		Bot.on('messageCreate', async message => {
 			if (message.author.bot || answered3) return;
-			// Todo: Base 64 query insert?
-			// Todo: serverAddQuery ?
-			const json = message.content.replace(/(\r\n|\n|\r)/gm, "");
-			const queryInsert = [name, json];
-			queries.push(queryInsert);
+			let temp = btoa(message.content);
+			let link = "http://localhost:4321/query/" + name + "/" + temp;
 			answered3 = true;
-			message.channel.send("The query: " + queryInsert[0] + " was added successfully!!")
-			handleDiscordChat();
+			fetch(link, {
+				method: 'PUT',
+				body: "",
+				headers: { 'Content-Type': 'application/json'}
+				}).then(res => {
+				message.channel.send("The query: " + name + " was added successfully!!")
+				handleDiscordChat();
+			});
 		});
 	}
 
 	function handleListQuery() {
-		// Todo: serverListQueries?
-		message.channel.send("Loading currently added queries... ");
-		for (let i = 0; i < queries.length; i++) {
-			message.channel.send("[" + (i + 1) + "] " + queries[i][0]);
-		}
-		message.channel.send("Complete!");
+		let link = "http://localhost:4321/queries";
+		fetch(link).then(res => {
+			res.json().then(res1 => {
+				let result = res1.result;
+				for (let i = 0; i < result.length; i++) {
+					message.channel.send("- " + result[i].toString());
+				}
+			});
+		});
 	}
 
 	function handleRunQuery() {
-		// Todo: serverListQueries?
-		// Todo: serverGetQuery?
-		// Todo: serverQueryDataset?
-		answered4 = false;
+		let answered4 = false;
 		handleListQuery();
-		message.channel.send("Please select a query to run: ");
-		Bot.on('message', async message => {
+		message.channel.send("Please select a query name to run: ");
+		Bot.on('messageCreate', async message => {
 			if(message.author.bot || answered4) return;
 			answered4 = true;
 			const args = message.content.split(/ +/)
 			const command = args.shift().toLowerCase();
 
-			for (let i = 0; i < queries.length; i++) {
-				if (command === String(i + 1)) {
+			let fs = require('fs');
+			let arr = [];
+			let files = fs.readdirSync("../data/jsonFiles");
+			for (let statsKey of files) {
+				arr.push(statsKey);
+			}
+
+			for (let i = 0; i < arr.length; i++) {
+				if (command === arr[i].toLowerCase()) {
 					answered4 = true;
-					return requestQueryResults(queries[i])
+					return requestQueryResults(arr[i])
 				}
 			}
 		});
 	}
 
 	function requestQueryResults(query) {
-		message.channel.send("You have selected the query: " + query[0]);
-		message.channel.send("Not Implemented Yet");
+		message.channel.send("You have selected the query: " + query);
 		// Todo: run the actual query and send back feedback
+		let link = "http://localhost:4321/dataset/query/" + query;
+		// console.log(link);
+		fetch(link).then(res => {
+			// console.log(res);
+			res.json().then(res1 => {
+				console.log(res1);
+				// let result = res1.result.toString();
+				// message.channel.send(result);
+				message.channel.send("Complete!");
+			});
+		});
 	}
 
 	function handleDiscordChat() {
